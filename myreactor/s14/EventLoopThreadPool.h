93a056e4241710a68da376718d6cf538b3add903
minm923 <minm923@126.com>
2017-09-01 09:04:05 +0800

    add EventLoopThreadPool

---
+++ myreactor/s14/EventLoopThreadPool.h
@@ -0,0 +1,41 @@
+#ifndef MUDUO_NET_EVENTLOOPTHREADPOOL_H
+#define MUDUO_NET_EVENTLOOPTHREADPOOL_H
+
+#include "thread/Condition.h"
+#include "thread/Mutex.h"
+#include "thread/Thread.h"
+
+#include <vector>
+#include <boost/noncopyable.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+
+
+
+namespace muduo
+{
+
+class EventLoop;    
+class EventLoopThread;
+
+class EventLoopThreadPool : boost::noncopyable
+{
+public:
+    EventLoopThreadPool(EventLoop* baseLoop);
+    ~EventLoopThreadPool();
+    void setThreadNum(int numThreads) { numThreads_ = numThreads; }
+    void start();
+    EventLoop* getNextLoop();
+
+
+private:
+    EventLoop* baseLoop_;
+    bool started_;
+    int numThreads_;
+    int next_; // always in loop thread
+    boost::ptr_vector<EventLoopThread> threads_;
+    std::vector<EventLoop*> loops_;
+};
+
+}
+
+#endif
